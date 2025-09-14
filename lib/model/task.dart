a37651e688b6f6c006e22a2cc2af5434eb355155
Vince Melia <vince@drifta.co.uk>
2025-09-14 12:51:57 +0100

    Reformatted/

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -1,22 +1,10 @@
-class Task{
+class Task {
   final int id;
   final String name;
   final String? description;
   final bool completed;
 
-  Task({
-    required this.id,
-    required this.name,
-    this.description,
-    this.completed = false,
-  });
+  Task({required this.id, required this.name, this.description, this.completed = false});
 
-  Map<String, Object?> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'description': description,
-      'completed': completed,
-    };
-  }
+  Map<String, Object?> toJson() => {'id': id, 'name': name, 'description': description, 'completed': completed};
 }
